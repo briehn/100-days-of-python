6089cd1fc2fac55638ffb154786992268db9defa
Briehn <briehnyu@gmail.com>
2025-04-03 13:20:33 -0400

    Day 1 Complete

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,1 +1,4 @@
-# Write your code below this line 👇
+print("Hello! Welcome to BNGP!")
+city = input("What city did you grow up in?")
+pet = input("Got a pet name?")
+print("Your band name is: " + city + pet + "!")
