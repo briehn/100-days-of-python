196d261454e651828782a2889857ad74b1375f56
Briehn <briehnyu@gmail.com>
2025-04-05 17:02:22 -0400

    Day 4 Finish

--- Day 4/Rock Paper Scissors/task.py
+++ Day 4/Rock Paper Scissors/task.py
@@ -1,3 +1,5 @@
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -24,3 +26,36 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+
+rps = [rock, paper, scissors]
+choice = input("Rock, paper, or scissors? ").lower()
+rand = random.randint(0, len(rps) - 1)
+randChoice = ""
+
+print("You chose:")
+
+if choice == "rock":
+    print(rock)
+elif choice == "paper":
+    print(paper)
+else:
+    print(scissors)
+
+print("Opponent chose:")
+
+if rand == 0:
+    print(rock)
+    randChoice = "rock"
+elif rand == 1:
+    print(paper)
+    randChoice = "paper"
+else:
+    print(scissors)
+    randChoice = "scissors"
+
+if (choice == "rock" and randChoice == "paper") or (choice == "paper" and randChoice == "rock") or (choice == "scissor" and randChoice == "rock"):
+    print("You win")
+elif choice == randChoice:
+    print("Tie")
+else:
+    print("You lose")
